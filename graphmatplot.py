a8bd72fe1aa740067789eba779b0d63a44bdf694
Vishnu Prasad <vishnuprasadvp1996@gmail.com>
2017-06-21 22:01:35 +0530

    Update graphmatplot.py

--- graphmatplot.py
+++ graphmatplot.py
@@ -1,5 +1,5 @@
 import numpy as np
-csv=np.genfromtxt('ans.csv',delimiter=",", dtype=None)
+csv=np.genfromtxt('ans.csv',delimiter=",", dtype=None)  #Converting csv file into array for easier manipulation
 
 import matplotlib.pyplot as plt
 plt.plot(csv)
